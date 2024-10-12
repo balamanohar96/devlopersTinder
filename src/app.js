94e59260bd72f6114b74ea23dcff6dfbe5c7f518
bala manohar <balamanohar.yeminedi@gmail.com>
2024-10-12 06:21:50 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -3,44 +3,86 @@ const adminAuth = require("./middleware/auth");
 const connectDB = require("./config/database");
 const UserCollectionModel = require("./models/user");
 const app = express();
+app.use(express.json());
 
-app.post("/signup", async (req, res) => {
-  const newUserInfo = {
-    age: 4,
-    firstName: "sai",
-    emailID: "sai@gmail.com",
-    mobile: 76545456,
-  };
-  const newDocument = new UserCollectionModel(newUserInfo);
-
+app.get("/feed", async (req, res) => {
   try {
-    await newDocument.save();
-    res.send("user created successfully");
+    const users = await UserCollectionModel.find({});
+    res.send(users);
   } catch (err) {
-    res.status(500).send("connection to database failed");
+    res.status(500).send("Cannot get feed data " + err.message);
   }
 });
 
-app.get("/user/:id", (req, res) => {
+app.get("/user", async (req, res) => {
+  const firstName = req.body.firstName;
+  try {
+    if (!firstName) {
+      throw new Error("Please enter firstName");
+    }
+    const users = await UserCollectionModel.find({ firstName: firstName });
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Cannot get user data. " + err.message);
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const newUserInfo = req.body;
+  try {
+    const newDocument = new UserCollectionModel(newUserInfo);
+    await newDocument.save();
+    res.send("user created successfully");
+  } catch (err) {
+    res
+      .status(500)
+      .send("something went wrong while creating a new user. " + err.message);
+  }
+});
+
+app.patch("/user/:userID", async (req, res) => {
+  const reqObj = req.body;
+  const userId = req.params.userID;
+  const allowedUpdates = [
+    "about",
+    "skills",
+    "age",
+    "gender",
+    "mobile",
+    "lastName",
+    "password",
+  ];
+  const isUpdateAllowed = Object.keys(reqObj).every((each) =>
+    allowedUpdates.includes(each)
+  );
+  try {
+    if (!isUpdateAllowed) {
+      throw new Error("Some of the entries cannot be updated!");
+    }
+    await UserCollectionModel.findByIdAndUpdate(userId, reqObj);
+    res.send("User updated successfully.");
+  } catch (err) {
+    res
+      .status(500)
+      .send("Something went wrong while updating the user. " + err.message);
+  }
+});
+
+app.get("/bala/:id", (req, res) => {
   console.log(req.params);
   res.send({ id: req.params.id, city: "guntur" });
 });
 
-app.get("/user", (req, res) => {
-  if (req.query.brand) {
-    console.log(req.query);
+app.get("/bala", (req, res) => {
+  if (req.query?.brand) {
     res.send({ firstName: "bala", lastName: "manohar", laptop: req.query });
   } else {
-    res.send({ firstName: "bala", lastName: "manohar" });
+    res.send([{ firstName: "bala", lastName: "manohar" }]);
   }
 });
 
 app.get("/admin", adminAuth, (req, res) => {
   res.send("admin");
-});
-
-app.post("/user", (req, res) => {
-  res.send("user created successfully");
 });
 
 app.use("/", (err, req, res, next) => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,90 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userCollectionSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userCollectionSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      maxLength: 30,
+      trim: true,
+    },
+    emailID: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      maxLength: 50,
+      lowercase: true,
+      validate(value) {
+        const isValid = validator.isEmail(value);
+        if (!isValid) {
+          throw new Error("Invalid emailId");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      maxLength: 70,
+      trim: true,
+      validate(value) {
+        const isStrong = validator.isStrongPassword(value);
+        if (!isStrong) {
+          throw new Error("Enter strong password");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 15,
+      max: 120,
+    },
+    gender: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        const validOptions = ["male", "female"];
+        const isValid = validOptions.includes(value);
+        if (!isValid) {
+          throw new Error("Please enter valid gender");
+        }
+      },
+    },
+    skills: {
+      type: [],
+      default: [],
+      validate(arr) {
+        if (arr.length > 10) {
+          throw new Error("Cannot add more than 10 skills");
+        }
+      },
+    },
+    mobile: {
+      type: Number,
+      validate(value) {
+        if (value.toString().length != 10) {
+          throw new Error("Please provide a valid mobile number");
+        }
+      },
+    },
+    about: {
+      type: String,
+      maxLength: 30,
+      trim: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailID: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  mobile: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 const UserCollectionModel = mongoose.model("user", userCollectionSchema);
 //  !                      "collection name" (No caps) (singular pronounce)
